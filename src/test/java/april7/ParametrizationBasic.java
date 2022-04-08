20612daccc003cef12187f72c7b439d21aaef878
Duotech B4 <duotechb3@gmail.com>
2022-04-07 20:01:24 -0400

    April 7 Part 2

--- src/test/java/april7/ParametrizationBasic.java
+++ src/test/java/april7/ParametrizationBasic.java
@@ -1,11 +1,51 @@
 package april7;
 
-import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.time.Duration;
 
 public class ParametrizationBasic {
 
 
 
+
+
+
+    @Test
+    @Parameters ({"SEARCH_WORD", "BROWSER_TYPE"})
+    public void verifySearchTerm(String searchTerm, String browserType) throws InterruptedException {
+        WebDriver driver = null;
+
+        if(browserType.equals("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }else if (browserType.equals("edge")){
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }else if(browserType.equals("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys(searchTerm, Keys.ENTER);
+        Thread.sleep(1000);
+
+        Assert.assertTrue(driver.getTitle().contains(searchTerm));
+        driver.quit();
+    }
 
 
 
